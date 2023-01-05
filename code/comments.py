7a9b0fcab9209eb43fec4e3d11330ce5a0727f05
shuklaavinash <avinashshukla.com>
2023-01-05 15:11:28 +0100

    first commit

---
+++ code/comments.py
@@ -0,0 +1,28 @@
+# Comments can be used to explain the code. Comments can be used to make the code more readable.
+
+# Comments starts with # in Python.
+
+# For Example
+#This is a comment
+print("Coding Without talking. - 1")
+
+
+# Comments can be placed at the end of a line.
+
+# For Example
+print("Coding Without talking. - 2") #This is a comment at the end of a line.
+
+# Multiline Comments.
+# Python does not have a syntax for multiline comments.
+# To add a multiline comment you should insert # for each line.
+
+# For Example
+# Comment line 1
+# Comment line 2
+# Comment line 3
+print("Coding Without talking. - 3")
+
+
+# Shortcut to comment or uncomment in Visual Studio Code:
+# Ctrl + /
+
